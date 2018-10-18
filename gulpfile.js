60f3273f949ddf192d98d8c45cbfc51a4f3214c4
Nikita PB <npb.nikita@gmail.com>
2018-10-18 17:20:47 +0300

    Init refactoring p.4
    -Add assets-source folder for un-optimized images
    -Minify all images
    -Add webp versions for all .png and .jpg
    -Add modernizr for background-images web-p support
    -Minify svg
    -Minify favicon from 83kb to 33kb

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+var gulp = require(`gulp`);
+
+// ######################## //
+// DEVELOPMENT images START //
+// ####################### //
+
+var imagemin = require(`gulp-imagemin`);
+var webp = require(`gulp-webp`);
+var svgstore = require(`gulp-svgstore`);
+var rename = require(`gulp-rename`);
+var imageminPngquant = require(`imagemin-pngquant`);
+var imageminGuetzli = require('imagemin-guetzli');
+var cssmin = require(`gulp-csso`);
+var run = require(`run-sequence`);
+var del = require(`del`);
+var uglify = require(`gulp-uglify-es`).default;
+
+// Minify png
+gulp.task(`pngmin`, () =>
+  gulp.src(`assets-source/**/*.png`)
+  .pipe(imagemin([
+    imageminPngquant({ quality: 65 })
+  ]))
+  .pipe(gulp.dest(`assets`))
+)
+
+// Minify jpg and make them progressive
+gulp.task('jpgmin', () =>
+  gulp.src('assets-source/**/*.jpg')
+  .pipe(imagemin([
+    imageminGuetzli({ quality: 85 }),
+    imagemin.jpegtran({ progressive: true })
+  ]))
+  .pipe(gulp.dest('assets'))
+);
+
+// Minify SVG
+gulp.task(`svgo`, function() {
+  gulp.src(`assets-source/**/*.svg`)
+    .pipe(imagemin([
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest(`assets`));
+});
+
+// Genetare SVG sprite from icons starting with `icon-`
+gulp.task(`sprite`, function() {
+  gulp.src(`assets-source/icon-*.svg`)
+    .pipe(imagemin([
+      imagemin.svgo()
+    ]))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename(`sprite.svg`))
+    .pipe(gulp.dest(`assets`));
+});
+
+gulp.task(`webp`, function() {
+  return gulp.src(`assets-source/**/*.{png,jpg}`)
+    .pipe(webp({ quality: 65 }))
+    .pipe(gulp.dest(`assets/webp`));
+});
+
+
+// ###################### //
+// DEVELOPMENT images END //
+// ###################### //
+
+
+// --------------------------------------------//
+
+
+// ##### //
+// Build //
+// ##### //
+
+gulp.task(`copy`, function() {
+  return gulp.src([
+      `*.html`,
+      `favicon.ico`
+    ], {
+      base: `.`
+    })
+    .pipe(gulp.dest(`public`));
+});
+
+gulp.task(`clean`, function() {
+  return del(`public`);
+});
+
+gulp.task(`cssmin`, function() {
+  gulp.src(`css/style.css`)
+    .pipe(cssmin())
+    .pipe(gulp.dest(`public/css`));
+});
+
+gulp.task(`jsmin`, function() {
+  gulp.src(`js/*.js`)
+    .pipe(uglify())
+    .pipe(gulp.dest(`public/js`));
+});
+
+gulp.task(`build`, function(done) {
+  run(`clean`, `copy`, `pngmin`, `jsmin`, `cssmin`, `svgo`, done);
+});
